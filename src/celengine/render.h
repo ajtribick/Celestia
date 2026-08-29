4a8a83c6eb219a13b770562cd96582901d668e66
Andrew Tribick <ajtribick@googlemail.com>
2026-08-30 00:01:18 +0200

    Use range-based algorithms

--- src/celengine/render.h
+++ src/celengine/render.h
@@ -201,9 +201,9 @@ class Renderer
 
     // GL wrappers
     void getViewport(int* x, int* y, int* w, int* h) const;
-    void getViewport(std::array<int, 4>& viewport) const;
+    void getViewport(std::span<int, 4> viewport) const;
     void setViewport(int x, int y, int w, int h);
-    void setViewport(const std::array<int, 4>& viewport);
+    void setViewport(std::span<const int, 4> viewport);
     void setScissor(int x, int y, int w, int h);
     void removeScissor();
 
@@ -354,8 +354,6 @@ class Renderer
         LabelHorizontalAlignment halign : 3;
         LabelVerticalAlignment valign : 3;
         float size;
-
-        std::partial_ordering operator<=>(const Annotation&) const;
     };
 
     void addForegroundAnnotation(const celestia::MarkerRepresentation* markerRep,
@@ -409,9 +407,6 @@ class Renderer
         const Star* star;
         Eigen::Vector3d origin;
         float opacity;
-
-        friend bool operator==(const OrbitPathListEntry&, const OrbitPathListEntry&) = default;
-        std::partial_ordering operator<=>(const OrbitPathListEntry&) const;
     };
 
     enum class FontStyle : std::uint8_t
